18524edecf92a964b89c5a3fa9a3f82bd54edd74
Avi977 <maharjanabhilekh@gmail.com>
2024-01-23 14:32:36 -0800

    .

--- Choose your adventure.py
+++
@@ -1,6 +0,0 @@
-#LLMs like CHAT GPT are very bad at recalling stuff
-#We want to INJECT the data into the model and let the answers be based on those previous prompts
-#Retrieval Agumented Generation (RAG) RLLM
-#requires a super fast DB so we used a free vector Database by DATASTAX ie; ASTRA CASSANDRA DB instead of a tabular DB
-
-#AstraCS:MFiNBQgWUapeCBlEDEfYotas:0593a1c71f7e0e6cdcc7ab0b177fbbba12e21dc36855054a68fd835af592bb0d
